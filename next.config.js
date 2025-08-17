0c022884752ec965729cdaf79bb30f5b97a0d2f8
Regi <registubbs@gmail.com>
2025-08-17 13:54:55 +0200

    second try

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: { styledComponents: true },
+  output: "export",
+  images: { unoptimized: true }, // nodig omdat image-optimalisatie niet werkt met export
 };
 module.exports = nextConfig;
